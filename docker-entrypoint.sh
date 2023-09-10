2606ff8c18795c3014b567724e73c5be5b4e1ab8
meenachinmay <chinmayanand896@gmail.com>
2023-09-11 07:26:01 +0900

    updated docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ echo "Running migrations..."
 
 # Run migrations for development
 # npx prisma migrate dev --name init --preview-feature
-npx prisma migrate deploy --preview-feature
+# npx prisma migrate deploy --preview-feature
 
 echo "Ran the migrations"
 
