2cf29519ddbefd2ad90780671c8a515bd8e020b3
meenachinmay <Chinmayanand896@gmail.com>
2023-09-10 15:45:10 +0900

    added docker-files-template folder to contains raw docker files and cleaned up main docker-compose.yml file

---
+++ prisma/migrations/20230907044059_init/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "images" TEXT NOT NULL,
+    "tags" TEXT NOT NULL,
+    "activities" TEXT NOT NULL,
+    "omiyage" TEXT NOT NULL,
+    "snsLinks" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "prefecture" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
