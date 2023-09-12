c3e309d76acd421a60de9b61f2746a3421162487
meenachinmay <chinmayanand896@gmail.com>
2023-09-12 23:40:57 +0900

    tests added for create event

--- src/event/event.service.spec.ts
+++ src/event/event.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { EventService } from './event.service';
+import { EventsService as EventService } from './event.service';
+import { PrismaService } from '../prisma.service';
 
 describe('EventService', () => {
   let service: EventService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EventService],
+      providers: [EventService, PrismaService],
     }).compile();
 
     service = module.get<EventService>(EventService);
@@ -14,5 +15,28 @@ describe('EventService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create an event', async () => {
+    const createEventDto = {
+      id: 1,
+      title: 'Test Title',
+      description: 'Test Description',
+      images: [
+        { url: 'http://url.com/image' },
+        { url: 'http://urlb.com/image' },
+      ],
+      tags: ['tag1', 'tag2'],
+      activities: ['sport', 'action'],
+      omiyage: ['choco', 'cake'],
+      snsLinks: ['http://sns1.com', 'http://sns2.com'],
+      city: 'Osaka',
+      prefecture: 'Osaka',
+      createdAt: new Date(),
+    };
+    const createdEvent = await service.createEvent(createEventDto);
+
+    expect(createdEvent).toBeDefined();
+    expect(createEventDto.title).toBe(createEventDto.title);
   });
 });
